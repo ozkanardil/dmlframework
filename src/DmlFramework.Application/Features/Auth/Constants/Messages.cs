b3938b21475268eb1b5e1c33396c03c7e90cc5a4
Özkan Ardil <ozkanardil@gmail.com>
2023-06-26 22:05:42 +0300

    Auth fluent validation has been completed

--- src/DmlFramework.Application/Features/Auth/Constants/Messages.cs
+++ src/DmlFramework.Application/Features/Auth/Constants/Messages.cs
@@ -8,8 +8,16 @@ namespace DmlFramework.Application.Features.Auth.Constants
 {
     public static class Messages
     {
+        // Validation error messages
+        public static string NotValidEmailAddress = "Please enter a valid email address.";
+        public static string PasswordCanNotBeNullOrEmpty = "Password s required.x";
+        public static string PasswordLengthIsNotValid = "Password length have to be 4-12 characters long.";
+
+        
         public static string UserNotFound = "User not found.";
         public static string UserLoginOk = "Login succesfull.";
         public static string UserLoginErr = "Login error.";
+
+
     }
 }
