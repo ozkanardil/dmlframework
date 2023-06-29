d030e82d8c56897703c030c145d5763ec24812c0
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 18:05:26 +0300

    UserRole create and delete validator classes have been added

---
+++ src/DmlFramework.Api/Validators/UserRoleFeatureValidators/CreateUserRoleCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using DmlFramework.Application.Features.User.Commands;
+using DmlFramework.Application.Features.User.Constants;
+using DmlFramework.Application.Features.UserRole.Command;
+using DmlFramework.Application.Shared.Constants;
+using FluentValidation;
+
+namespace DmlFramework.Api.Validators.UserRoleFeatureValidators
+{
+    public class CreateUserRoleCommandValidator : AbstractValidator<CreateUserRoleCommand>
+    {
+        public CreateUserRoleCommandValidator()
+        {
+            RuleFor(c => c.userRoleId).GreaterThan(0).WithMessage(SharedMassages.InvalidId);
+            RuleFor(c => c.userEmail).EmailAddress().NotNull().NotEmpty().WithMessage(SharedMassages.InvalidEmailAddress);
+        }
+    }
+}
---
+++ src/DmlFramework.Api/Validators/UserRoleFeatureValidators/DeleteUserRoleCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using DmlFramework.Application.Features.UserRole.Command;
+using DmlFramework.Application.Shared.Constants;
+using FluentValidation;
+
+namespace DmlFramework.Api.Validators.UserRoleFeatureValidators
+{
+    public class DeleteUserRoleCommandValidator : AbstractValidator<DeleteUserRoleCommand>
+    {
+        public DeleteUserRoleCommandValidator()
+        {
+            RuleFor(c => c.Id).GreaterThan(0).WithMessage(SharedMassages.InvalidId);
+        }
+    }
+}
