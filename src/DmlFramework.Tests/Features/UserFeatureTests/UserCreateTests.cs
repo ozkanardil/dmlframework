69d505210a9fe6e376b87011976f25448faaf25d
Özkan Ardil <ozkanardil@gmail.com>
2023-06-29 18:56:24 +0300

    One parameter has been added for UserCreate test

--- src/DmlFramework.Tests/Features/UserFeatureTests/UserCreateTests.cs
+++ src/DmlFramework.Tests/Features/UserFeatureTests/UserCreateTests.cs
@@ -88,6 +88,7 @@ namespace DmlFramework.Tests.Features.UserFeatureTests
 
         [Theory]
         [InlineData(1, "Name-1", "Surname-1", "test1@test.com", "1111", 1)]
+        [InlineData(2, "Name-2", "Surname-2", "test2@test.com", "2222", 1)]
         public async Task User_Create_Existance_Check_Test(int userId, string userName, string userSurname, string userEmail, string userPassword, int userStatus)
         {
             // Arrange
